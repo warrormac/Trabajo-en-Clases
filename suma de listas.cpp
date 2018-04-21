2ef28ff7b1eaee7c32e5d82f70e15c628e88c51f
warrormac <andre.alvarez@ucsp.edu.pe>
2018-04-21 16:33:45 -0700

    Update suma de listas.cpp

--- suma de listas.cpp
+++ suma de listas.cpp
@@ -2,24 +2,38 @@
 
 using namespace std;
 
-int sum(int x[])
+
+
+int len(int x[])
+{
+    int cont=0;
+    for (int y=0;x[y]!='\0';y++)
+        cont=cont+1;
+    return cont;
+}
+
+
+int sum(int x[],int y)
 {
     int z=0;
-    for (int y=0;y<5;y++)
+    int cont=0;
+    while (cont<y)
     {
-        z=z+x[y];
+        z+=x[cont];
+        cont++;
     }
     cout<<z;
 }
 
+
+
 int main()
 {
-    int a[5];
-    cout<<"ingrese sus valores: \n";
-    for (int x=0;x<5;x++)
-    {
-        cin>>a[x];
-    }
-    sum(a);
-
+    int a[]={1,2,3,4,5,6,7,8,9};
+    int leng=0;
+    leng=len(a);
+    cout<<"el tamaño de la lista es de:"<<leng<<"\n\n";
+    sum(a,leng);
 }
+
+
