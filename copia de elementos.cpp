066ad192680682e4e5cc892cdda49d8e6db5c7ac
warrormac <andre.alvarez@ucsp.edu.pe>
2018-04-21 17:03:37 -0700

    Update copia de elementos.cpp

--- copia de elementos.cpp
+++ copia de elementos.cpp
@@ -2,24 +2,40 @@
 
 using namespace std;
 
-int copia(int x[], int y)
-{
-    int r[y];
-    for (int z=0;z<y;z++)
-    {
-        r[z]=x[z];
-        cout<<r[z];
-    }
 
+
+int len(int x[])
+{
+    int cont=0;
+    for (int y=0;x[y]!='\0';y++)
+        cont=cont+1;
+    return cont;
 }
+
+
+int cambio(int x[],  int p)
+{
+    int l=0;
+    int y[p];
+    while (l<p)
+    {
+        y[l]=x[l];
+
+        l++;
+
+    }
+    cout<<"la copia de la cadena x es"<<" y[";
+    for (int cont=0;cont<p;cont++)
+        cout<<y[cont];
+    cout<<"]";
+}
+
 int main()
 {
-    int b=5;
-    int a[b];
-    cout<<"ingrese sus valores: \n";
-    for (int x=0;x<b;x++)
-    {
-        cin>>a[x];
-    }
-    copia(a,b);
+    int a[]={1,2,3,4,5,6,7,8,9};
+    int leng=0;
+    leng=len(a);
+    cout<<"el tamaño de la lista es de:"<<leng<<"\n\n";
+    cambio(a,leng);
 }
+
