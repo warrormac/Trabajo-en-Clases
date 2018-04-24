4cd716de666ab815906440fe1ea7bc6ae75c941a
warrormac <andre.alvarez@ucsp.edu.pe>
2018-04-24 10:54:51 -0500

    Update copia de elementos.cpp

--- copia de elementos.cpp
+++ copia de elementos.cpp
@@ -12,32 +12,33 @@ int tam(char x[])
     return cont;
 }
 
-int cambio(char x[],  int p)
+void cambio(char x[],  char y[],int p)
 {
     int l=0;
-    char y[p];
     while (l<p)
     {
-        y[l]=x[l];
+        x[l]=y[l];
 
         l++;
 
     }
     cout<<"la copia de la cadena x es"<<" y[";
     for (int cont=0;cont<p;cont++)
-        cout<<y[cont];
+        cout<<x[cont];
     cout<<"]";
 }
 
 
 int main()
 {
-    char a[]={'h','o','l','a','\0'};
+    char a[]={'h','o','l','a',' ','\0'};
+    char b[]={'a','n','d','r','e','\0'};
     int leng=0;
     leng=tam(a);
     cout<<"el tamaño de la lista es de:"<<leng<<"\n\n";
-    cambio(a,leng);
+    cambio(a,b,leng);
 }
+
 
 
 
